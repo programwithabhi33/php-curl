cba29842b6c7af0a6edd041b323cce943d854a8a
abhi <programwithabhi@gmail.com>
2022-09-29 23:48:04 +0530

    empty commit

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 
+// Initialising the curl library with the curl_init() method
 $abhi = curl_init();
-curl_setopt($abhi,CURLOPT_URL,"https://www.google.com");
+
+// Set the options using the curl_setopt() method
+curl_setopt($abhi,CURLOPT_URL,"https://www.amazon.in/s?k=gaming+laptops");
+
+// This line for the bypass the https dont verify the ssl certificate
+curl_setopt($abhi,CURLOPT_SSL_VERIFYPEER,true);
+
+// Executing the curl
 curl_exec($abhi);
+
+// Closing the connection 
 curl_close($abhi);
 
 
