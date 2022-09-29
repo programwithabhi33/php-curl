412b33bdbf6a7971634268f6843a003af881c4db
abhi <programwithabhi@gmail.com>
2022-09-30 00:03:14 +0530

    empty commit

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ curl_setopt($abhi,CURLOPT_URL,"https://www.amazon.in/s?k=gaming+laptops");
 // This line for the bypass the https dont verify the ssl certificate
 curl_setopt($abhi,CURLOPT_SSL_VERIFYPEER,true);
 
+// This line help you to store the response in the vairable
+curl_setopt($abhi,CURL_RETURNTRANSFER,true);
+
 // Executing the curl
 curl_exec($abhi);
 
